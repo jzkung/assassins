ca0e4aec8b34fff4aa1aea5c9a349306ebc646ec
Jessica Kung <jzkung@gmail.com>
2014-04-23 22:49:45 -0700

    Set up rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Assassins::Application.config.secret_key_base = '5e6816ddb8aee1262671552a1ebe255dd1fa18c9d94ea74459881543772ebad927d4cde826258fec0b345e745612c1382efd8f9698f18cd0f12429e1c5984069'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Assassins::Application.config.session_store :cookie_store, key: '_assassins_session'
