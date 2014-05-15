54434a45bb49307a5b45440960cdbd9c73eb4ddc
Simon Shachter <simonys@stanford.edu>
2014-05-15 12:05:42 -0700

    naively extended UserMailer, works on registration, have not implemented other emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,8 +1,26 @@
 class UserMailer < ActionMailer::Base
   default from: "jro.assassins@gmail.com"
 
-  def welcome_email
-  	mail(to: "jzkung@gmail.com", subject: "test")
+  def welcome_email(user)
+  	@user = user
+  	mail(to: @user.email, subject: "Welcome to Assassins!")
+  end
+
+  def game_welcome(game, user)
+  	@user = user
+  	@game = game
+  	mail(to: @user.email, subject: "You've Signed Up for a Game")
+  end
+
+  def kill_confirm(kills, user)
+  	@user = user
+  	@kills = kills
+  	mail(to: @user.email, subject: "You're New Assignment")
+  end
+
+  def win_email(user)
+  	@user = user
+  	mail(to: @user.email, subject: "You've Won!!")
   end
 
 end
