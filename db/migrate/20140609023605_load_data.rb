91056ded33f7d72d82913e72f32759f77ca42f41
Jessica Kung <jzkung@gmail.com>
2014-06-08 19:39:30 -0700

    replaced load_data

--- db/migrate/20140609023605_load_data.rb
+++ db/migrate/20140609023605_load_data.rb
@@ -37,64 +37,67 @@ class LoadData < ActiveRecord::Migration
     emily = User.new(:first_name => "Emily", :last_name => "Nguyen", :name => "Emily Nguyen", :email => "xinnn@live.com", :role => "player", :status => "alive", :kill_code => "kill", :provider => 'facebook', :uid => '813487398661335', :image_url => 'http://graph.facebook.com/813487398661335/picture?type=large', :profile_url => 'https://www.facebook.com/813487398661335')
     emily.game = game2
     emily.save(:validate => false)
-    
-    kill1 = Kill.new(:lat => "37.427755", :lng => "-122.166958")
+
+    date = DateTime.now.utc-8.hour
+
+    kill1 = Kill.new(:lat => "37.427755", :lng => "-122.166958", :time_killed => date)
     kill1.game = game1
     kill1.target = xin
     kill1.assassin = stari
     kill1.save(:validate => false)
-    kill2 = Kill.new(:lat => "37.426835", :lng => "-122.17049")
+    kill2 = Kill.new(:lat => "37.426835", :lng => "-122.17049", :time_killed => date)
     kill2.game = game1
     kill2.target = stari
     kill2.assassin = simone
     kill2.save(:validate => false)
-    kill3 = Kill.new(:lat => "37.422381", :lng => "-122.176005")
+    kill3 = Kill.new(:lat => "37.422381", :lng => "-122.176005", :time_killed => date)
     kill3.game = game1
     kill3.target = simone
     kill3.assassin = princess
     kill3.save(:validate => false)
-    kill4 = Kill.new(:lat => "37.429817", :lng => "-122.169272")
+    kill4 = Kill.new(:lat => "37.429817", :lng => "-122.169272", :time_killed => date)
     kill4.game = game1
     kill4.target = princess
     kill4.assassin = dhruti
     kill4.save(:validate => false)
-    kill5 = Kill.new(:lat => "37.426982", :lng => "-122.167566")
+    kill5 = Kill.new(:lat => "37.426982", :lng => "-122.167566", :time_killed => date)
     kill5.game = game1
     kill5.target = dhruti
     kill5.assassin = sophie
     kill5.save(:validate => false)
-    kill6 = Kill.new(:lat => "37.430612", :lng => "-122.171488")
+    kill6 = Kill.new(:lat => "37.430612", :lng => "-122.171488", :time_killed => date)
     kill6.game = game1
     kill6.target = sophie
     kill6.assassin = emily
     kill6.save(:validate => false)
 
-    message1 = Message.new(:post_date => "", :content => "Watch Out for Me!")
+
+    message1 = Message.new(:post_date => date, :content => "Watch Out for Me!")
     message1.player = xin
     message1.game = game1
     message1.save(:validate => false)
-    message2 = Message.new(:post_date => "", :content => "Don't let your guard down!")
+    message2 = Message.new(:post_date => date, :content => "Don't let your guard down!")
     message2.player = dhruti
     message2.game = game1
     message2.save(:validate => false)
-    message3 = Message.new(:post_date => "", :content => "I'm coming for you and your roommate!")
+    message3 = Message.new(:post_date => date, :content => "I'm coming for you and your roommate!")
     message3.player = sophie
     message3.game = game1
     message3.save(:validate => false)
-    message4 = Message.new(:post_date => "", :content => "Don't f*** around with this s***")
+    message4 = Message.new(:post_date => date, :content => "Don't f*** around with this s***")
     message4.player = princess
     message4.game = game1
     message4.save(:validate => false)
-    message5 = Message.new(:post_date => "", :content => "I'm DYING!!!")
+    message5 = Message.new(:post_date => date, :content => "I'm DYING!!!")
     message5.player = xin
     message5.game = game1
     message5.save(:validate => false)
 
-    message6 = Message.new(:post_date => "", :content => "Are you ready for this!?")
+    message6 = Message.new(:post_date => date, :content => "Are you ready for this!?")
     message6.player = stari
     message6.game = game2
     message6.save(:validate => false)
-    message7 = Message.new(:post_date => "", :content => "You're all gonna die!")
+    message7 = Message.new(:post_date => date, :content => "You're all gonna die!")
     message7.player = stari
     message7.game = game2
     message7.save(:validate => false)
@@ -104,5 +107,6 @@ class LoadData < ActiveRecord::Migration
   def down
       User.delete_all
       Game.delete_all
+      Message.delete_all
   end
 end
